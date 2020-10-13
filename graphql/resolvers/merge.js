afeb5801177effd47eb08346c90e2a6b7181a19b
Dromediansk <miroslav.pillar@senacor.com>
2020-10-13 19:30:51 +0200

    batch jobs for event and user

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
+const DataLoader = require("dataloader");
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
+
+const eventLoader = new DataLoader((eventIds) => {
+  return fetchEventlistByIds(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const fetchEventlistByIds = async (eventIds) => {
   try {
@@ -15,8 +24,8 @@ const fetchEventlistByIds = async (eventIds) => {
 
 const findEventById = async (eventId) => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
@@ -24,11 +33,11 @@ const findEventById = async (eventId) => {
 
 const findUserById = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: fetchEventlistByIds.bind(this, user._doc.createdEvents),
+      createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents),
     };
   } catch (err) {
     throw err;
